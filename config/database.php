c30f54baa16f6ed90a356f31195c7ae76fdb01e0
Pablo <royadeveloper01@gmail.com>
2020-02-06 10:58:45 +0000

    table migrate

--- config/database.php
+++ config/database.php
@@ -52,17 +52,17 @@ return [
             'password' => 'chidinma01',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
+            // 'prefix' => '',
             'strict' => false,
             'engine' => null,
-            'modes' =>[
-                        'ONLY_FULL_GROUP_BY',
-                        'STRICT_TRANS_TABLES,
-                        NO_ZERO_IN_DATE,
-                        NO_ZERO_DATE,
-                        ERROR_FOR_DIVISION_BY_ZERO,
-                        NO_ENGINE_SUBSTITUTION,'
-                    ],
+            // 'modes' =>[
+            //             'ONLY_FULL_GROUP_BY',
+            //             'STRICT_TRANS_TABLES,
+            //             NO_ZERO_IN_DATE,
+            //             NO_ZERO_DATE,
+            //             ERROR_FOR_DIVISION_BY_ZERO,
+            //             NO_ENGINE_SUBSTITUTION,'
+            //         ],
             
         ],
 
